4554b609795d05e8371fd0eaa22f445ab767ec08
Srinithyayedugani <srinithyayedugani@gmail.com>
2025-10-19 22:22:25 +0530

    Update Lab2P1.java

--- LAB2/Lab2P1.java
+++ LAB2/Lab2P1.java
@@ -1,27 +1,33 @@
-import java.util.lang;
-
+import java.util.Scanner;
 class Lab2P1{
-	public static void main(String... args){
-	Scanner s= new Scanner(System.in);
-	int a;
-	string op;
-	s.nextInt();
-	s.nextString();
-		switch(op){
-		case1 "++":
-			System.out.println("pre inc:"+(++a)+"\t"+"post inc:"(a++));
-			break;
-		case2 "--":
-			System.out.println("pre dec:"+(--a)+"/t"+"pos dec:"(a--));
-			break;
-		case3 "+":
-			a=+a;
-			System.out.println("positive sign bit:"+ (a));
-			break;
-		case4 "-":
-			a=-a;
-			System.out.println(unary -:"+ (a));
-			break;
-		}
-	}
-}	
+	public static void main(String...args){
+	
+	Scanner s = new Scanner(System.in);
+	int a = s.nextInt();
+	s.nextLine();
+	String op = s.nextLine();
+	switch(op){
+	case "++":
+		    System.out.println("Post : "+(a++));
+		    System.out.println("Pre : "+(++a));
+		    break;
+	case "--":
+		    System.out.println("Post : "+(a--));
+		    System.out.println("Pre : "+(--a));
+		    break;
+	case "+":
+	            a = +a;
+	            System.out.println("Unary +:"+(a));
+		    break;
+	case "-":
+	            a = -a;
+	            System.out.println("Unary +:"+(-a));
+		    break;
+	case "~":   
+	            System.out.println("Unary +:"+(~a));
+		    break;
+	default :   	 
+                    System.out.println("sorry you entered wrong operator");
+}
+}
+}
